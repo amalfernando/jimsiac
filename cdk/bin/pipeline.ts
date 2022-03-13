fb596c591ca2c7cd7bfd2c88a4a84e316a00c37f
Amal Fernando <amal.fernando@jims.net>
2022-03-14 06:23:38 +1100

    Added pipeline stack

--- cdk/bin/pipeline.ts
+++ cdk/bin/pipeline.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { TestStack } from '../lib/test-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new TestStack(app, 'TestStack', {
+new PipelineStack(app, 'PipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
